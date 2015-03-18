e9ae50747785fb1954a64837de2c862f42900d29
Matej Sekoranja <matej.sekoranja@cosylab.com>
2015-03-18 13:40:24 +0100

    initial commit; starting new API

---
+++ src/main/java/org/epics/Channel.java
@@ -0,0 +1,25 @@
+package org.epics;
+
+import java.util.concurrent.Future;
+
+public interface Channel<T> {
+	
+	// non-finished, placeholder definitions
+	public class Status {};
+	public interface Data<T> { public T getValue(); };
+	public interface Metadata<T> extends Data<T> {};
+	public interface TimeStamp<T> extends Metadata<T> { public long getTimeStamp(); };
+	
+	
+	public Future<Void> connect();
+	
+	public T get();
+	public void put(T value);
+	
+	public Future<T> getAsync();
+	public Future<Status> putAsync();
+	
+	public <VT extends Metadata<T>> VT get(Class<VT> clazz);
+	public <VT extends Metadata<T>> Future<VT> getAsync(Class<VT> clazz);
+	
+}
---
+++ src/test/java/org/epics/test/ChannelTest.java
@@ -0,0 +1,34 @@
+package org.epics.test;
+
+import java.util.concurrent.Future;
+
+import org.epics.Channel;
+import org.epics.Channel.TimeStamp;
+
+
+public class ChannelTest {
+	
+	
+	public static void main(String[] args) throws Throwable {
+		
+		Channel<Double> adc = null; // = new Channel<Double>("adc01");
+		
+		// sync get
+		double value = adc.get();
+		
+		// sync get w/ timestamp 
+		TimeStamp<Double> ts = adc.get(TimeStamp.class);
+		value = ts.getValue();
+		long millis = ts.getTimeStamp();
+		
+		// best-effort put
+		adc.put(12.3);
+		
+		// async get
+		Future<Double> fd = adc.getAsync();
+		
+		// in some other thread
+		value = fd.get();
+		
+	}
+}
