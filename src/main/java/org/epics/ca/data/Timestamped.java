c8bf6658e97acb0d337371a5156c3d929c7f727c
Matej Sekoranja <matej.sekoranja@cosylab.com>
2015-04-22 10:28:53 +0200

    timestamped implemented

--- src/main/java/org/epics/ca/data/Timestamped.java
+++ src/main/java/org/epics/ca/data/Timestamped.java
@@ -1,6 +1,31 @@
 package org.epics.ca.data;
 
-// TODO
 public class Timestamped<T> extends Alarm<T> {
-	public long getTimeStamp() { return 0; }
+
+	// private static long EPOCH_SECONDS_PAST_1970 = 7305*86400L;
+
+	/** past 1.1.2970 */
+	protected long seconds;
+	protected int nanos;
+
+	public long getSeconds() {
+		return seconds;
+	}
+
+	public void setSeconds(long seconds) {
+		this.seconds = seconds;
+	}
+
+	public int getNanos() {
+		return nanos;
+	}
+
+	public void setNanos(int nanos) {
+		this.nanos = nanos;
+	}
+	
+	public long getMillis()
+	{
+		return seconds * 1000 + nanos / 1000000;
+	}
 }
