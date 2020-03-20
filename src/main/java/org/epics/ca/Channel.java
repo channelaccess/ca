6296baa1b01848524d7f29a5dc7a627953587326
rees_s <simon.rees@psi.ch>
2020-03-20 14:22:00 +0100

    Issue #64 Refactor project according to IntelliJ inspection suggestions.

--- src/main/java/org/epics/ca/Channel.java
+++ src/main/java/org/epics/ca/Channel.java
@@ -6,30 +6,28 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import org.epics.ca.data.Metadata;
-import org.epics.ca.util.Holder;
 
-import com.lmax.disruptor.dsl.Disruptor;
 
 public interface Channel<T> extends AutoCloseable
 {
-   public String getName();
+   String getName();
 
-   public ConnectionState getConnectionState();
+   ConnectionState getConnectionState();
 
-   public AccessRights getAccessRights();
+   AccessRights getAccessRights();
 
-   public Channel<T> connect();
+   Channel<T> connect();
 
-   public CompletableFuture<Channel<T>> connectAsync();
+   CompletableFuture<Channel<T>> connectAsync();
 
 
    //
    // listeners
    //
 
-   public Listener addConnectionListener( BiConsumer<Channel<T>, Boolean> handler );
+   Listener addConnectionListener( BiConsumer<Channel<T>, Boolean> handler );
 
-   public Listener addAccessRightListener( BiConsumer<Channel<T>, AccessRights> handler );
+   Listener addAccessRightListener( BiConsumer<Channel<T>, AccessRights> handler );
 
 
    //
@@ -37,23 +35,23 @@ public interface Channel<T> extends AutoCloseable
    //
 
    // NOTE: reusable get methods
-   //	public T get(T reuse);
-   // public CompletableFuture<T> getAsync(T reuse);
-   // public <MT extends Metadata<T>> MT get(Class<? extends Metadata> clazz, T reuse);
-   // public <MT extends Metadata<T>> CompletableFuture<MT> getAsync(Class<? extends MT> clazz, T reuse);
+   //	T get(T reuse);
+   // CompletableFuture<T> getAsync(T reuse);
+   // <MT extends Metadata<T>> MT get(Class<? extends Metadata> clazz, T reuse);
+   // <MT extends Metadata<T>> CompletableFuture<MT> getAsync(Class<? extends MT> clazz, T reuse);
 
-   public T get();
+   T get();
 
-   public void put( T value );
+   void put( T value );
 
-   public void putNoWait( T value ); // best-effort put
+   void putNoWait( T value ); // best-effort put
 
    //
    // async methods, exception is reported via CompletableFuture
    //
-   public CompletableFuture<T> getAsync();
+   CompletableFuture<T> getAsync();
 
-   public CompletableFuture<Status> putAsync( T value );
+   CompletableFuture<Status> putAsync( T value );
 
    // NOTE: "public <MT extends Metadata<T>> MT get(Class<MT> clazz)" would
    // be a better definition, however it raises unchecked warnings in the code
@@ -62,10 +60,10 @@ public interface Channel<T> extends AutoCloseable
    // (it will raise ClassCastException if not properly used)
 
    @SuppressWarnings( "rawtypes" )
-   public <MT extends Metadata<T>> MT get( Class<? extends Metadata> clazz );
+   <MT extends Metadata<T>> MT get( Class<? extends Metadata> clazz );
 
    @SuppressWarnings( "rawtypes" )
-   public <MT extends Metadata<T>> CompletableFuture<MT> getAsync( Class<? extends Metadata> clazz );
+   <MT extends Metadata<T>> CompletableFuture<MT> getAsync( Class<? extends Metadata> clazz );
 
    //
    // monitors
@@ -78,7 +76,7 @@ public interface Channel<T> extends AutoCloseable
    }
 
    // Value-only monitor. User-specified notification mask.
-   public Monitor<T> addValueMonitor( Consumer<? super T> handler, int mask );
+   Monitor<T> addValueMonitor( Consumer<? super T> handler, int mask );
 
    // Metadata monitor.  Default, value-change, notification mask.
    @SuppressWarnings( "rawtypes" )
@@ -89,13 +87,11 @@ public interface Channel<T> extends AutoCloseable
 
    // Metadata monitor.  User-specified notification mask.
    @SuppressWarnings( "rawtypes" )
-   public <MT extends Metadata<T>> Monitor<MT> addMonitor( Class<? extends Metadata> clazz, Consumer<MT> handler, int mask );
-
+   <MT extends Metadata<T>> Monitor<MT> addMonitor( Class<? extends Metadata> clazz, Consumer<MT> handler, int mask );
 
    //
    // misc
    //
-
    // get channel properties, e.g. native type, host, etc.
    Map<String, Object> getProperties();
 
