14b1fbbd7a76c1597581f49847684c823382b6f6
rees_s <simon.rees@psi.ch>
2020-03-24 21:34:39 +0100

    Issue #64 Refactor project according to IntelliJ inspection suggestions.

--- src/main/java/org/epics/ca/data/Timestamped.java
+++ src/main/java/org/epics/ca/data/Timestamped.java
@@ -3,7 +3,7 @@ package org.epics.ca.data;
 public class Timestamped<T> extends Alarm<T>
 {
 
-   public static long EPOCH_SECONDS_PAST_1970 = 7305 * 86400L;
+   public static final long EPOCH_SECONDS_PAST_1970 = 7305 * 86400L;
 
    /**
     * past 1.1.2970
