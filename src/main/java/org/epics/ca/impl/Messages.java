4bf818ba955c053e29e144cdc0242dded6c776b9
rees_s <simon.rees@psi.ch>
2020-03-24 21:41:15 +0100

    Issue #64 Refactor project according to IntelliJ inspection suggestions.

--- src/main/java/org/epics/ca/impl/Messages.java
+++ src/main/java/org/epics/ca/impl/Messages.java
@@ -3,6 +3,7 @@ package org.epics.ca.impl;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 
+import org.apache.commons.lang3.Validate;
 import org.epics.ca.Constants;
 import org.epics.ca.impl.TypeSupports.TypeSupport;
 import org.epics.ca.util.net.InetAddressUtil;
@@ -39,41 +40,44 @@ public final class Messages
                                             short dataType, int dataCount,
                                             int parameter1, int parameter2 )
    {
-      boolean useExtendedHeader = payloadSize >= 0xFFFF || dataCount >= 0xFFFF;
+      Validate.notNull( transport );
+
+      final boolean useExtendedHeader = payloadSize >= 0xFFFF || dataCount >= 0xFFFF;
 
       // check if supported by current transport protocol revision
-      if ( useExtendedHeader && transport != null && transport.getMinorRevision () < 9 )
-         throw new IllegalArgumentException ("Out of bounds.");
+      if ( useExtendedHeader && transport.getMinorRevision () < 9 )
+      {
+         throw new IllegalArgumentException( "Out of bounds." );
+      }
 
-      int requiredSize = useExtendedHeader ?
+      final int requiredSize = useExtendedHeader ?
             Constants.CA_EXTENDED_MESSAGE_HEADER_SIZE :
             Constants.CA_MESSAGE_HEADER_SIZE;
 
-      ByteBuffer buffer = transport.acquireSendBuffer (requiredSize + payloadSize);
+      final ByteBuffer buffer = transport.acquireSendBuffer (requiredSize + payloadSize);
 
       // standard header
+      buffer.putShort( command );
       if ( !useExtendedHeader )
       {
-         buffer.putShort (command);
          // conversion int -> unsigned short is done right
-         buffer.putShort ((short) payloadSize);
-         buffer.putShort (dataType);
+         buffer.putShort( (short) payloadSize);
+         buffer.putShort( dataType);
          // conversion int -> unsigned short is done right
-         buffer.putShort ((short) dataCount);
-         buffer.putInt (parameter1);
-         buffer.putInt (parameter2);
+         buffer.putShort( (short) dataCount);
+         buffer.putInt( parameter1 );
+         buffer.putInt( parameter2 );
       }
       // extended header
       else
       {
-         buffer.putShort (command);
-         buffer.putShort ((short) 0xFFFF);
-         buffer.putShort (dataType);
-         buffer.putShort ((short) 0x0000);
-         buffer.putInt (parameter1);
-         buffer.putInt (parameter2);
-         buffer.putInt (payloadSize);
-         buffer.putInt (dataCount);
+         buffer.putShort( (short) 0xFFFF );
+         buffer.putShort( dataType );
+         buffer.putShort( (short) 0x0000 );
+         buffer.putInt( parameter1 );
+         buffer.putInt( parameter2 );
+         buffer.putInt( payloadSize );
+         buffer.putInt( dataCount);
       }
 
       return buffer;
@@ -86,9 +90,12 @@ public final class Messages
     * @param buffer the buffer.
     * @return result, always true.
     */
-   public static final boolean generateRepeaterRegistration( ByteBuffer buffer )
+   @SuppressWarnings( "UnusedReturnValue" )
+   public static boolean generateRepeaterRegistration( ByteBuffer buffer )
    {
-      int localAddress = InetAddressUtil.ipv4AddressToInt (InetAddress.getLoopbackAddress ());
+      Validate.notNull( buffer );
+
+      final int localAddress = InetAddressUtil.ipv4AddressToInt (InetAddress.getLoopbackAddress ());
 
       buffer.putShort ((short) 24);
       // conversion int -> unsigned short is done right
@@ -113,8 +120,12 @@ public final class Messages
     *
     * @return success status.
     */
-   public static final boolean generateSearchRequestMessage( Transport transport, ByteBuffer buffer, String name, int cid )
+   public static boolean generateSearchRequestMessage( Transport transport, ByteBuffer buffer, String name, int cid )
    {
+      Validate.notNull( transport );
+      Validate.notNull( buffer );
+      Validate.notNull( name );
+
       // name length was already validated at channel creation time
 
       int unalignedMessageSize = Constants.CA_MESSAGE_HEADER_SIZE + name.length () + 1;
@@ -127,9 +138,9 @@ public final class Messages
       buffer.putShort ((short) (alignedMessageSize - Constants.CA_MESSAGE_HEADER_SIZE));
       buffer.putShort (Constants.CA_SEARCH_DONTREPLY);
       // conversion int -> unsigned short is done right
-      buffer.putShort ((short) transport.getMinorRevision ());
-      buffer.putInt (cid);
-      buffer.putInt (cid);
+      buffer.putShort( transport.getMinorRevision () );
+      buffer.putInt( cid );
+      buffer.putInt( cid );
 
       // append zero-terminated string and align message
       buffer.put (name.getBytes ());
@@ -150,17 +161,20 @@ public final class Messages
     * @param isSequenceNumberValid boolean switch which determines whether sequence number
     *        or priority argument is used in the formatting of the message.
     */
-   public static final void generateVersionRequestMessage( Transport transport, ByteBuffer buffer, short priority,
-                                                           int sequenceNumber, boolean isSequenceNumberValid )
+   public static void generateVersionRequestMessage( Transport transport, ByteBuffer buffer, short priority,
+                                                     int sequenceNumber, boolean isSequenceNumberValid )
    {
-      short isSequenceNumberValidCode = isSequenceNumberValid ? (short) 1 : (short) 0;
+      Validate.notNull( transport );
+      Validate.notNull( buffer );
+
+      final short isSequenceNumberValidCode = isSequenceNumberValid ? (short) 1 : (short) 0;
 
       buffer.putShort ((short) 0);
       // conversion int -> unsigned short is done right
       buffer.putShort ((short) 0);
       buffer.putShort (isSequenceNumberValid ? isSequenceNumberValidCode : priority);
       // conversion int -> unsigned short is done right
-      buffer.putShort ((short) transport.getMinorRevision ());
+      buffer.putShort( transport.getMinorRevision() );
       buffer.putInt (sequenceNumber);
       buffer.putInt (0);
    }
@@ -171,31 +185,33 @@ public final class Messages
     * @param transport the transport.
     * @param buffer the buffer.
     */
-   public static final void generateEchoMessage( Transport transport, ByteBuffer buffer )
+   public static void generateEchoMessage( Transport transport, ByteBuffer buffer )
    {
+      Validate.notNull( transport );
+      Validate.notNull( buffer );
+
       if ( transport.getMinorRevision () < 4 )
       {
-         buffer.putShort ((short) 23);
+         buffer.putShort( (short) 23 );
          // conversion int -> unsigned short is done right
-         buffer.putShort ((short) 0);
-         buffer.putShort ((short) 0);
+         buffer.putShort( (short) 0 );
+         buffer.putShort( (short) 0 );
          // conversion int -> unsigned short is done right
-         buffer.putShort ((short) 0);
-         buffer.putInt (0);
-         buffer.putInt (0);
+         buffer.putShort( (short) 0 );
       }
       else
       {
          // use CA version message as echo message
-         buffer.putShort ((short) 0);
+         buffer.putShort( (short) 0 );
          // conversion int -> unsigned short is done right
-         buffer.putShort ((short) 0);
-         buffer.putShort ((short) 0);
+         buffer.putShort( (short) 0 );
+         buffer.putShort( (short) 0 );
          // conversion int -> unsigned short is done right
-         buffer.putShort ((short) transport.getMinorRevision ());
-         buffer.putInt (0);
-         buffer.putInt (0);
+         buffer.putShort( transport.getMinorRevision ());
       }
+
+      buffer.putInt( 0 );
+      buffer.putInt( 0 );
    }
 
    /**
@@ -209,18 +225,20 @@ public final class Messages
     */
    public static void versionMessage( Transport transport, short priority, int sequenceNumber, boolean isSequenceNumberValid )
    {
+      Validate.notNull( transport );
+
       boolean ignore = true;
       try
       {
-         short isSequenceNumberValidCode = isSequenceNumberValid ? (short) 1 : (short) 0;
+         final short isSequenceNumberValidCode = isSequenceNumberValid ? (short) 1 : (short) 0;
 
-         startCAMessage (transport,
-                         (short) 0,
-                         0,
-                         isSequenceNumberValid ? isSequenceNumberValidCode : priority,
-                         (short) transport.getMinorRevision (),
-                         sequenceNumber,
-                         0);
+         startCAMessage ( transport,
+                          (short) 0,
+                          0,
+                          isSequenceNumberValid ? isSequenceNumberValidCode : priority,
+                          transport.getMinorRevision (),
+                          sequenceNumber,
+                          0);
          ignore = false;
       }
       finally
@@ -237,9 +255,13 @@ public final class Messages
     */
    public static void hostNameMessage( Transport transport, String hostName )
    {
+      Validate.notNull( transport );
+
       // compatibility check
       if ( transport.getMinorRevision () < 1 )
+      {
          return;
+      }
 
       int unalignedMessageSize = Constants.CA_MESSAGE_HEADER_SIZE + hostName.length () + 1;
       int alignedMessageSize = calculateAlignedSize (8, unalignedMessageSize);
@@ -247,7 +269,7 @@ public final class Messages
       boolean ignore = true;
       try
       {
-         ByteBuffer buffer = startCAMessage (transport,
+         final ByteBuffer buffer = startCAMessage (transport,
                                              (short) 21,
                                              alignedMessageSize - Constants.CA_MESSAGE_HEADER_SIZE,
                                              (short) 0,
@@ -257,6 +279,7 @@ public final class Messages
 
          // append zero-terminated string and align message
          buffer.put (hostName.getBytes ());
+
          // terminate with 0 and pad
          for ( int i = alignedMessageSize - unalignedMessageSize + 1; i > 0; i-- )
             buffer.put ((byte) 0);
@@ -277,17 +300,22 @@ public final class Messages
     */
    public static void userNameMessage( Transport transport, String userName )
    {
+      Validate.notNull( transport );
+      Validate.notNull( userName );
+
       // compatibility check
       if ( transport.getMinorRevision () < 1 )
+      {
          return;
+      }
 
-      int unalignedMessageSize = Constants.CA_MESSAGE_HEADER_SIZE + userName.length () + 1;
-      int alignedMessageSize = calculateAlignedSize (8, unalignedMessageSize);
+      final int unalignedMessageSize = Constants.CA_MESSAGE_HEADER_SIZE + userName.length () + 1;
+      final int alignedMessageSize = calculateAlignedSize (8, unalignedMessageSize);
 
       boolean ignore = true;
       try
       {
-         ByteBuffer buffer = startCAMessage (transport,
+         final ByteBuffer buffer = startCAMessage (transport,
                                              (short) 20,
                                              alignedMessageSize - Constants.CA_MESSAGE_HEADER_SIZE,
                                              (short) 0,
@@ -299,7 +327,9 @@ public final class Messages
          buffer.put (userName.getBytes ());
          // terminate with 0 and pad
          for ( int i = alignedMessageSize - unalignedMessageSize + 1; i > 0; i-- )
-            buffer.put ((byte) 0);
+         {
+            buffer.put((byte) 0);
+         }
 
          ignore = false;
       }
@@ -318,6 +348,8 @@ public final class Messages
     */
    public static void createChannelMessage( Transport transport, String channelName, int cid )
    {
+      Validate.notNull( transport );
+
       // v4.4+ or newer
       if ( transport.getMinorRevision () < 4 )
       {
@@ -327,15 +359,17 @@ public final class Messages
 
       int binaryNameLength = 0;
       if ( channelName != null )
-         binaryNameLength = channelName.length () + 1;
+      {
+         binaryNameLength = channelName.length() + 1;
+      }
 
-      int unalignedMessageSize = Constants.CA_MESSAGE_HEADER_SIZE + binaryNameLength;
-      int alignedMessageSize = calculateAlignedSize (8, unalignedMessageSize);
+      final int unalignedMessageSize = Constants.CA_MESSAGE_HEADER_SIZE + binaryNameLength;
+      final int alignedMessageSize = calculateAlignedSize (8, unalignedMessageSize);
 
       boolean ignore = true;
       try
       {
-         ByteBuffer buffer = startCAMessage (transport,
+         final ByteBuffer buffer = startCAMessage ( transport,
                                              (short) 18,
                                              alignedMessageSize - Constants.CA_MESSAGE_HEADER_SIZE,
                                              (short) 0,
@@ -349,7 +383,9 @@ public final class Messages
             buffer.put (channelName.getBytes ());
             // terminate with 0 and pad
             for ( int i = alignedMessageSize - unalignedMessageSize + 1; i > 0; i-- )
-               buffer.put ((byte) 0);
+            {
+               buffer.put((byte) 0);
+            }
          }
 
          ignore = false;
@@ -372,6 +408,8 @@ public final class Messages
     */
    public static void readNotifyMessage( Transport transport, int dataType, int dataCount, int sid, int ioid )
    {
+      Validate.notNull( transport );
+
       boolean ignore = true;
       try
       {
@@ -403,6 +441,8 @@ public final class Messages
     */
    public static void createSubscriptionMessage( Transport transport, int dataType, int dataCount, int sid, int ioid, int mask )
    {
+      Validate.notNull( transport );
+
       boolean ignore = true;
       try
       {
@@ -439,10 +479,10 @@ public final class Messages
     * @param sid the CA Server ID.
     * @param ioid the CA message IOID.
     */
-   public static void cancelSubscriptionMessage(
-         Transport transport, int dataType, int dataCount, int sid, int ioid
-   )
+   public static void cancelSubscriptionMessage( Transport transport, int dataType, int dataCount, int sid, int ioid )
    {
+      Validate.notNull( transport );
+
       boolean ignore = true;
       try
       {
@@ -469,10 +509,10 @@ public final class Messages
     * @param cid the CA Client ID.
     * @param sid the CA Server ID.
     */
-   public static void clearChannelMessage(
-         Transport transport, int cid, int sid
-   )
+   public static void clearChannelMessage( Transport transport, int cid, int sid )
    {
+      Validate.notNull( transport );
+
       boolean ignore = true;
       try
       {
@@ -503,6 +543,8 @@ public final class Messages
     */
    public static void subscriptionUpdateMessage( Transport transport, int dataType, int dataCount, int sid, int ioid )
    {
+      Validate.notNull( transport );
+
       boolean ignore = true;
       try
       {
@@ -535,13 +577,15 @@ public final class Messages
     */
    public static <T> void writeMessage( Transport transport, int sid, int cid, TypeSupport<T> typeSupport, T value, int count )
    {
-      int calculatedPayloadSize = typeSupport.serializeSize (value, count);
-      int alignedPayloadSize = calculateAlignedSize (8, calculatedPayloadSize);
+      Validate.notNull( transport );
+
+      final int calculatedPayloadSize = typeSupport.serializeSize (value, count);
+      final int alignedPayloadSize = calculateAlignedSize (8, calculatedPayloadSize);
 
       boolean ignore = true;
       try
       {
-         ByteBuffer buffer = startCAMessage (transport,
+         final ByteBuffer buffer = startCAMessage (transport,
                                              (short) 4,
                                              alignedPayloadSize,
                                              (short) typeSupport.getDataType (),
@@ -553,7 +597,9 @@ public final class Messages
 
          // align
          for ( int i = alignedPayloadSize - calculatedPayloadSize; i > 0; i-- )
-            buffer.put ((byte) 0);
+         {
+            buffer.put((byte) 0);
+         }
 
          ignore = false;
       }
@@ -576,13 +622,15 @@ public final class Messages
     */
    public static <T> void writeNotifyMessage( Transport transport, int sid, int ioid, TypeSupport<T> typeSupport, T value, int count )
    {
-      int calculatedPayloadSize = typeSupport.serializeSize (value, count);
-      int alignedPayloadSize = calculateAlignedSize (8, calculatedPayloadSize);
+      Validate.notNull( transport );
+
+      final int calculatedPayloadSize = typeSupport.serializeSize (value, count);
+      final int alignedPayloadSize = calculateAlignedSize (8, calculatedPayloadSize);
 
       boolean ignore = true;
       try
       {
-         ByteBuffer buffer = startCAMessage (transport,
+         final ByteBuffer buffer = startCAMessage (transport,
                                              (short) 19,
                                              alignedPayloadSize,
                                              (short) typeSupport.getDataType (),
@@ -634,6 +682,8 @@ public final class Messages
     */
    public static void eventsOnSubscriptionMessage(Transport transport)
    {
+      Validate.notNull( transport );
+
       boolean ignore = true;
       try
       {
