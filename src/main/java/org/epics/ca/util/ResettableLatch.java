b4357d46ea819f93273c41a920ad7ce815fa2a99
rees_s <simon.rees@psi.ch>
2020-03-24 21:32:43 +0100

    Issue #64 Refactor project according to IntelliJ inspection suggestions.

--- src/main/java/org/epics/ca/util/ResettableLatch.java
+++ src/main/java/org/epics/ca/util/ResettableLatch.java
@@ -1,10 +1,9 @@
-/**
- * Based on CountDownLatch code, it adds reset() method.
- * Written by Doug Lea with assistance from members of JCP JSR-166
- * Expert Group and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
+/*
+  Based on CountDownLatch code, it adds reset() method.
+  Written by Doug Lea with assistance from members of JCP JSR-166
+  Expert Group and released to the public domain, as explained at
+  http://creativecommons.org/publicdomain/zero/1.0/
  */
-
 package org.epics.ca.util;
 
 import java.util.concurrent.CyclicBarrier;
