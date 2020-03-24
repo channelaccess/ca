e278e760b94f6ce0e027bc10390951b9fabb70b0
rees_s <simon.rees@psi.ch>
2020-03-24 21:31:45 +0100

    Issue #64 Refactor project according to IntelliJ inspection suggestions.

--- src/main/java/org/epics/ca/util/Holder.java
+++ src/main/java/org/epics/ca/util/Holder.java
@@ -3,11 +3,7 @@ package org.epics.ca.util;
 public class Holder<T>
 {
    public T value;
-
-   public Holder()
-   {
-   }
-
+   public Holder() {}
    public Holder( T value )
    {
       this.value = value;
