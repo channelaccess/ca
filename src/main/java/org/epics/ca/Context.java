902577ae295d640ea33af0df07d2a977ac13fa48
rees_s <simon.rees@psi.ch>
2020-05-08 03:47:12 +0200

    #74 Cleanup code formatting (no change of behaviour).

--- src/main/java/org/epics/ca/Context.java
+++ src/main/java/org/epics/ca/Context.java
@@ -1,30 +1,52 @@
+/*- Package Declaration ------------------------------------------------------*/
+
 package org.epics.ca;
 
-import java.util.Properties;
+/*- Imported packages --------------------------------------------------------*/
 
+import java.util.Properties;
 import org.epics.ca.impl.ContextImpl;
+
+
+/*- Interface Declaration ----------------------------------------------------*/
+/*- Class Declaration --------------------------------------------------------*/
 
 public class Context implements AutoCloseable
 {
 
+/*- Public attributes --------------------------------------------------------*/
+
    public enum Configuration
    {
-      EPICS_CA_ADDR_LIST, EPICS_CA_AUTO_ADDR_LIST, EPICS_CA_CONN_TMO,
-      EPICS_CA_BEACON_PERIOD, EPICS_CA_REPEATER_PORT, EPICS_CA_SERVER_PORT,
+      EPICS_CA_ADDR_LIST,
+      EPICS_CA_AUTO_ADDR_LIST,
+      EPICS_CA_CONN_TMO,
+      EPICS_CA_BEACON_PERIOD,
+      EPICS_CA_REPEATER_PORT,
+      EPICS_CA_SERVER_PORT,
       EPICS_CA_MAX_ARRAY_BYTES
    }
 
+/*- Private attributes -------------------------------------------------------*/
+
    private final ContextImpl delegate;
+
+
+/*- Main ---------------------------------------------------------------------*/
+/*- Constructor --------------------------------------------------------------*/
 
    public Context()
    {
-      this (System.getProperties() );
+      this( System.getProperties() );
    }
 
    public Context( Properties properties )
    {
       delegate = new ContextImpl( properties );
    }
+
+
+/*- Public methods -----------------------------------------------------------*/
 
    public <T> Channel<T> createChannel( String channelName, Class<T> channelType )
    {
