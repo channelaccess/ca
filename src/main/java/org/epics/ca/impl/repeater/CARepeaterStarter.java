0d7e0e196c64048da9928c9621c6034aba56014f
rees_s <simon.rees@psi.ch>
2020-05-19 18:14:19 +0200

    Further miscellaneous cleanup.

--- src/main/java/org/epics/ca/impl/repeater/CARepeaterStarter.java
+++ src/main/java/org/epics/ca/impl/repeater/CARepeaterStarter.java
@@ -5,18 +5,20 @@ package org.epics.ca.impl.repeater;
 /*- Interface Declaration ----------------------------------------------------*/
 /*- Class Declaration --------------------------------------------------------*/
 
-import java.io.File;
-import java.io.IOException;
+import org.apache.commons.lang3.Validate;
+import org.epics.ca.Constants;
+import org.epics.ca.util.logging.LibraryLogManager;
+
+import java.io.*;
 import java.net.*;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.*;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Starts the CA repeater in a separate JVM instance.
+ * Provides mechanisms for starting the CA Repeater either directly from
+ * the command-line or in a separate JVM process via a library call.
  */
 public class CARepeaterStarter
 {
@@ -24,122 +26,218 @@ public class CARepeaterStarter
 /*- Public attributes --------------------------------------------------------*/
 /*- Private attributes -------------------------------------------------------*/
 
-private static final Logger logger = Logger.getLogger( CARepeaterStarter.class.getName () );
-
-static
-{
-   // force only IPv4 sockets, since EPICS does not work right with IPv6 sockets
-   // see http://java.sun.com/j2se/1.5.0/docs/guide/net/properties.html
-   System.setProperty ( "java.net.preferIPv4Stack", "true" );
-   CARepeaterUtils.initializeLogger( logger );
-}
-
-/**
- * System JVM property key to disable CA repeater.
- */
-public static final String CA_DISABLE_REPEATER = "CA_DISABLE_REPEATER";
+   private static final Logger logger = LibraryLogManager.getLogger( CARepeaterStarter.class );
 
 
 /*- Main ---------------------------------------------------------------------*/
+
+   /**
+    * Starts the CA Repeater from the command line.
+    *
+    * If at least two arguments are provided and the first argument is "-p"
+    * or "--port" then an attempt will be made to parse the second argument
+    * as the port number to use when starting the repeater.
+    *
+    * If the second argument is a valid port number (positive integer) then
+    * the repeater will be started on that port. If it isn't then the
+    * repeater will be started on the fallback repeater port.
+    *
+    * The fallback repeater port is the EPICS default repeater port (which
+    * depends on the CA Protocol Version) or the value specified by the
+    * system property 'EPICS_CA_REPEATER_PORT'.
+    *
+    * The default repeater port depends on the CA Protocol Version.
+    * For the version currently supported by this library CA Version 4.13
+    * the default repeater port is 5065.
+    *
+    * @param argv arguments.
+    */
+   public static void main( String[] argv )
+   {
+      if( ! NetworkUtilities.verifyTargetPlatformNetworkStackIsChannelAccessCompatible() )
+      {
+         System.exit( 128 );
+      }
+
+      logger.info( "The CA Repeater main method has been invoked with " + argv.length + " arguments." );
+      logger.info( "The arguments were: " + Arrays.toString( argv ) );
+
+      final boolean portArgumentSupplied = ( argv.length >= 2 && ( argv[ 0 ].equals ("-p") || argv[ 0 ].equals ("--port") ) );
+      final int fallbackRepeaterPort = parseToInt( System.getProperty( "EPICS_CA_REPEATER_PORT" ), Constants.CA_REPEATER_PORT );
+      final int port = portArgumentSupplied ? parseToInt( argv[ 1 ], fallbackRepeaterPort ) : fallbackRepeaterPort;
+
+      // Nothing to do, if a repeater instance is already running
+      if ( CARepeaterStarter.isRepeaterRunning( port ) )
+      {
+         logger.info( "The repeater is already running and a new instance will not be started." );
+         logger.info( "This process will now terminate." );
+         System.exit( 129 );
+      }
+
+      final CARepeater repeater;
+      try
+      {
+         {
+            logger.info( "Creating CA Repeater instance which will run on port " + port + "." );
+            repeater = new CARepeater( port );
+         }
+      }
+      catch( CARepeater.CaRepeaterStartupException ex )
+      {
+         logger.warning( "An exception occurred when attempting to start the repeater." );
+         logger.warning( "The exception message was: " + ex.getMessage() );
+         System.exit( 130 );
+         return;
+      }
+
+      // Run, run, run...
+      repeater.start();
+   }
+
+
 /*- Constructor --------------------------------------------------------------*/
 /*- Public methods -----------------------------------------------------------*/
 
    /**
-    * Attempts to starts the CA Repeater in a separate JVM instance if it is
-    * not already running.
+    * Starts the CA Repeater in a separate JVM process, if it is not already
+    * running.
     *
-    * If the CA Repeater is already running then this method simply returns
-    * without taking any further action.
-    *
-    * @param repeaterPort positive integer specifying the port to run it on.
-    *
-    * @throws IllegalArgumentException if the repeater port is invalid.
-    * @throws IllegalStateException if an attempt was made to start the
-    *    repeater but for some reason this was not possible. In these
-    *    circumstances further information is available in the log.
+    * @param repeaterPort specifies the port to listen on in the inclusive range 1-65535.
+    * @param debugEnabled whether debug logging should be enabled in the spawned
+    *    process.
+    * @throws IllegalArgumentException if the repeater port is out of range.
+    * @throws IllegalStateException if the repeater was already running.
+    * @throws IllegalStateException if some other unexpected condition prevents
+    *    repeater from being started.
     */
-   public static void spawnRepeaterIfNotAlreadyRunning( final int repeaterPort )
+   public static void startRepeaterIfNotAlreadyRunning( int repeaterPort, boolean debugEnabled )
    {
-      logger.finest( "Starting the repeater..." );
+      Validate.inclusiveBetween( 1, 65535, repeaterPort, "The port must be in the inclusive range 1-65535.");
 
-      // Validate input argument.
-      if ( repeaterPort < 1 )
+      if( ! NetworkUtilities.verifyTargetPlatformNetworkStackIsChannelAccessCompatible() )
       {
-         final String msg = "The repeater port must be a positive integer";
-         logger.warning( msg);
-         throw new IllegalArgumentException( msg );
+         throw new RuntimeException( "The network stack is incorrectly configured for EPICS CA protocol." );
       }
 
-      // Nothing to do, if a repeater instance is already running
-      logger.finest( "Checking whether the repeater is already running..." );
-      if ( isRepeaterRunning( repeaterPort ) )
+      synchronized ( CARepeaterStarter.class  )
       {
-         logger.info( "The repeater is already running and will not be started." );
-         return;
-      }
-      logger.finest( "The repeater is NOT already running so will be started now. " );
 
-      // Define a Privileged action which will start a separate JVM to run the repeater.
-      final PrivilegedAction<Optional<Throwable>> action = () ->
-      {
-         logger.finest( "Building CA Repeater command line..." );
-
-         // java.home java.class.path
-         final String[] commandLine = new String[] {
-               System.getProperty ("java.home") + File.separator + "bin" + File.separator + "java",
-               "-classpath",
-               System.getProperty( "java.class.path", "<java.class.path not found>"),
-               CARepeater.class.getName (),
-               "-p",
-               String.valueOf( repeaterPort )
-         };
-
-         try
+         if ( ! isRepeaterRunning( repeaterPort ) )
          {
-            logger.finest( "Attempting to run CA Repeater in separate process..." );
-            logger.finest( "Command Line: '" + Arrays.toString( commandLine ) + "'" );
-            final Process process = Runtime.getRuntime().exec( commandLine );
-            logger.finest( "Process was started." );
+            final Process process = startRepeaterInSeparateJvmProcess( repeaterPort, debugEnabled );
+            ProcessStreamConsumer.consumeFrom( process );
          }
-         catch ( IOException ex )
-         {
-            logger.log( Level.WARNING, "Failed to run '" + commandLine[ 0 ] + "' in separate process.", ex );
-            return Optional.of( ex );
-         }
-         return Optional.empty();
-      };
-
-      // Now run the privileged action
-      final Optional<Throwable> optionalResult = AccessController.doPrivileged(action );
-      if ( optionalResult.isPresent() )
-      {
-         final String msg = "Unable to start the CA Repeater. See the log for further details.";
-         logger.info( msg );
-         throw new IllegalStateException( msg, optionalResult.get() );
       }
-
-      logger.info( "The CA repeater was started on the local machine on port " + repeaterPort  + "." ) ;
    }
 
 /*- Package-level access methods ---------------------------------------------*/
 
    /**
-    * Check if a repeater is running bound to the wildcard instance on the local machine.
+    * Starts the CA Repeater, which should not already be running, in a separate
+    * JVM process. Returns the process handle which can subsequently be used
+    * to monitor the lifecycle of the process.
+    *
+    * @param repeaterPort specifies the port that the CA Repeater will listen on
+    *    in the range 1-65535.
+    * @param debugEnabled whether debug logging should be enabled in the spawned
+    *    process.
+    * @return process handle which can subsequently be used to monitor the process
+    *    and/or shut it down.
+    * @throws IllegalArgumentException if the repeater port is out of range.
+    * @throws IllegalStateException if the repeater was already running.
+    * @throws RuntimeException if some other unexpected condition prevents
+    *    repeater from being started.
+    */
+   static Process startRepeaterInSeparateJvmProcess( int repeaterPort, boolean debugEnabled )
+   {
+      Validate.inclusiveBetween( 1, 65535, repeaterPort, "The port must be in the inclusive range 1-65535.");
+      Validate.validState( ! isRepeaterRunning( repeaterPort ), "The repeater is already running on port." + repeaterPort );
+
+      logger.fine( "Starting the repeater in a separate process..." );
+
+      // Make an entry in the log of all local IPV4 network addresses.
+      logger.info( "The following local interfaces have been discovered..." ) ;
+      final List<Inet4Address> addrList = NetworkUtilities.getLocalNetworkInterfaceAddresses();
+      addrList.forEach( (ip) -> logger.info( ip.toString() ) );
+
+      final String classPath = System.getProperty("java.class.path", "<java.class.path not found>");
+      final String classWithMainMethod = CARepeaterStarter.class.getName();
+      final String repeaterPortAsString = Integer.toString( repeaterPort );
+      final List<String> commandLine = Arrays.asList( "java",
+                                                      "-cp", classPath,
+                                                      "-DCA_DEBUG=" + debugEnabled,
+                                                      "-Djava.net.preferIPv4Stack=true",
+                                                      "-Djava.net.preferIPv6Stack=false",
+                                                      classWithMainMethod,
+                                                      "-p", repeaterPortAsString );
+
+      try
+      {
+         // Spawn a new CA Repeater as a child of the existing process.
+         logger.finest( "Attempting to run CA Repeater in separate process using command line : '" + commandLine + "'." );
+         final Process process = new ProcessBuilder().command( commandLine ).start();
+
+         // Consume all output from it and send it to the log.
+         ProcessStreamConsumer.consumeFrom( process );
+         logger.finest( "The process was started OK" );
+         return process;
+      }
+      catch ( IOException ex )
+      {
+         final String message = "Failed to run '" + commandLine + "' in separate process.";
+         logger.log( Level.WARNING, message, ex );
+         throw new RuntimeException( message, ex );
+      }
+   }
+
+   /**
+    * Check if a repeater is running bound to any of the addresses associated
+    * with the local machine.
     *
     * @param repeaterPort repeater port.
     * @return <code>true</code> if repeater is already running, <code>false</code> otherwise
     */
    static boolean isRepeaterRunning( int repeaterPort )
    {
-      logger.log( Level.INFO, "Checking whether repeater is running on port " + repeaterPort );
+      logger.finest( "Checking whether repeater is running on port " + repeaterPort );
+
+      // The idea here is that binding to a port on thewildcard addreess we check all
+      // possible network interfaces on which the CA Repeater may be already running.
       final InetSocketAddress wildcardSocketAddress = new InetSocketAddress( repeaterPort );
-      final boolean repeaterIsRunning = ! SocketUtilities.isSocketAvailable( wildcardSocketAddress );
-      final String runningOrNot = repeaterIsRunning ? " IS " : " is NOT ";
-      logger.info( "The repeater on port " + repeaterPort + runningOrNot + "running." ) ;
+      logger.finest( "Checking socket address " + wildcardSocketAddress );
+      final boolean repeaterIsRunning = ! UdpSocketUtilities.isSocketAvailable( wildcardSocketAddress );
+      final String runningOrNot = repeaterIsRunning ? " IS " : " IS NOT ";
+      logger.finest( "The repeater on port " + repeaterPort + runningOrNot + "running." ) ;
       return repeaterIsRunning;
    }
 
 /*- Private methods ----------------------------------------------------------*/
+
+   /**
+    * Attempts to interpret the supplied string as an integer, returning
+    * the result if successful or otherwise some default value.
+    *
+    * @param stringToParse the input string.
+    * @param defaultValue the value to be returned if the  input string cannot
+    *    be parsed.
+    *
+    * @return the result
+    */
+   private static int parseToInt( String stringToParse, int defaultValue )
+   {
+      int ret;
+      try
+      {
+         ret = Integer.parseInt( stringToParse );
+      }
+      catch( NumberFormatException ex)
+      {
+         ret = defaultValue; //Use default value if parsing failed
+      }
+      return ret;
+   }
+
+
 /*- Nested Classes -----------------------------------------------------------*/
 
 }
