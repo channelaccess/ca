0657ae740edb7b94120e4e8db911dcb6b5c59fa3
rees_s <simon.rees@psi.ch>
2020-05-24 21:44:17 +0200

    Added support for finding broadcast addresses.

--- src/main/java/org/epics/ca/impl/repeater/NetworkUtilities.java
+++ src/main/java/org/epics/ca/impl/repeater/NetworkUtilities.java
@@ -5,14 +5,13 @@ package org.epics.ca.impl.repeater;
 /*- Interface Declaration ----------------------------------------------------*/
 /*- Class Declaration --------------------------------------------------------*/
 
+import org.apache.commons.lang3.Validate;
 import org.epics.ca.util.logging.LibraryLogManager;
 
 import java.net.*;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.logging.Level;
+import java.util.*;
 import java.util.logging.Logger;
+
 
 /**
  * Provides general-purpose, network-related helper functions for the CA Repeater.
@@ -33,25 +32,54 @@ public class NetworkUtilities
    {
       if ( verifyIpv4NetworkStackPreferred() && verifyIpv6NetworkStackNotPreferred() )
       {
-         System.out.println( "The network stack for this JVM is configured OK for channel access." );
+         logger.info( "The network stack for this JVM is configured OK for channel access." );
          return true;
       }
       else
       {
-         System.out.println( "JVM Configuration Error: the network stack for this JVM is NOT configured appropriately for channel access.");
+         logger.warning( "JVM Configuration Error: the network stack for this JVM is NOT configured appropriately for channel access." );
          if ( ! NetworkUtilities.verifyIpv4NetworkStackPreferred() )
          {
-            System.out.println( "Please ensure that the system property 'java.net.preferIPv4Stack' is set to true.");
+            logger.warning( "Please ensure that the system property 'java.net.preferIPv4Stack' is set to true." );
          }
          if ( ! NetworkUtilities.verifyIpv6NetworkStackNotPreferred() )
          {
-            System.out.println( "Please ensure that the system property 'java.net.preferIPv6Stack' is set to false.");
+            logger.warning( "Please ensure that the system property 'java.net.preferIPv6Stack' is set to false." );
          }
          return false;
       }
    }
 
 /*- Package-level methods ----------------------------------------------------*/
+
+   /**
+    * Returns an indication of whether a VPN is currently active on the local network.
+    *
+    * @return the indicator
+    */
+   static boolean isVpnActive()
+   {
+      final List<Inet4Address> addrList = new ArrayList<>();
+      try
+      {
+         for ( Enumeration<NetworkInterface> eni = NetworkInterface.getNetworkInterfaces(); eni.hasMoreElements(); )
+         {
+            final NetworkInterface nif = eni.nextElement();
+            // VPN's are typically implemented using Point-to-Point links.
+            if ( nif.isUp() && nif.isPointToPoint() )
+            {
+               logger.info( "An active VPN has been detected." );
+               return true;
+            }
+         }
+      }
+      catch ( SocketException e )
+      {
+         logger.warning( "Exception when querying local interfaces for registered IP4 addresses." );
+      }
+      logger.info( "An active VPN has NOT been detected." );
+      return false;
+   }
 
    /**
     * Returns a list of any IPV4 internet addresses that have been assigned to the local interfaces.
@@ -68,36 +96,90 @@ public class NetworkUtilities
       {
          for( Enumeration<NetworkInterface> eni = NetworkInterface.getNetworkInterfaces(); eni.hasMoreElements(); )
          {
-            final NetworkInterface ifc = eni.nextElement();
-            if( ifc.isUp() )
+            final NetworkInterface nif = eni.nextElement();
+
+            logger.finest( "name: " + nif.getName() );
+            logger.finest( "- isUp: " + nif.isUp() );
+            logger.finest( "- isPointToPoint: " + nif.isPointToPoint() );
+            logger.finest( "- isVirtual: " + nif.isVirtual() );
+            logger.finest( "- supportsMulticast: " + nif.supportsMulticast() );
+            logger.finest( "- isLoopback: " + nif.isLoopback() );
+
+            logger.finest( "- if addresses: " );
+            for ( InterfaceAddress ifAddr : nif.getInterfaceAddresses() )
             {
-               for (Enumeration<InetAddress> ena = ifc.getInetAddresses(); ena.hasMoreElements(); )
+               final InetAddress inetAddress = ifAddr.getAddress();
+               logger.finest( "  - addr: " + inetAddress);
+               logger.finest( "  - subnetMask: " + ifAddr.getNetworkPrefixLength() );
+               logger.finest( "  - broadcast: " + ifAddr.getBroadcast() );
+
+               if ( inetAddress instanceof Inet4Address )
                {
-                  final InetAddress addr = ena.nextElement();
-                  if ( addr instanceof Inet4Address)
-                  {
-                     addrList.add( (Inet4Address) addr );
-                  }
+                  Inet4Address inet4Address = (Inet4Address) inetAddress;
+                  addrList.add( inet4Address );
+               }
+            }
+            logger.finest( "" );
+         }
+      }
+      catch ( SocketException e )
+      {
+         logger.warning( "Exception when querying local interfaces for registered IP4 addresses." );
+      }
+      return addrList;
+   }
+
+   /**
+    * Returns a list of the broadcast enabled addresses associated with the local
+    * interfaces.
+    *
+    * Credit: the algorithm below is a modified form of the version described here:
+    * https://stackoverflow.com/questions/494465/how-to-enumerate-ip-addresses-of-all-enabled-nic-cards-from-java
+    *
+    * @return the list, empty if the query fails or if there are none available.
+    */
+   static List<Inet4Address> getLocalBroadcastAddresses()
+   {
+      final List<Inet4Address> addrList = new ArrayList<>();
+      try
+      {
+         for( Enumeration<NetworkInterface> eni = NetworkInterface.getNetworkInterfaces(); eni.hasMoreElements(); )
+         {
+            final NetworkInterface nif = eni.nextElement();
+            for ( InterfaceAddress ifAddr : nif.getInterfaceAddresses() )
+            {
+               final InetAddress inetAddress = ifAddr.getBroadcast();
+               if ( inetAddress != null   )
+               {
+                  addrList.add( (Inet4Address) inetAddress );
                }
             }
          }
       }
       catch ( SocketException e )
       {
-         logger.log(Level.WARNING, "Exception when querying local interfaces for registered IP4 addresses." );
+         logger.warning( "Exception when querying local interfaces for registered IP4 addresses." );
       }
       return addrList;
    }
 
    /**
-    * Returns an indication of whether the supplied IP address is
-    * valid for the local interface.
+    * Returns an indication of whether the supplied IP address is valid for
+    * the local interface.
+    *
+    * @implNote
+    * The wildcard address and loopback address(es) are always available.
+    * Additional local addresses are tested by interrogating the Java
+    * network stack.
     *
     * @param addr the address to check.
     * @return the result.
+    * @throws NullPointerException if the address argument was null.
     */
    static boolean isThisMyIpAddress( InetAddress addr )
    {
+      Validate.notNull( addr );
+
       // Check if the address is the wildcard address (0.0.0.0) or the loopback address (127.0.0.1)
       if ( addr.isAnyLocalAddress() || addr.isLoopbackAddress() )
       {
