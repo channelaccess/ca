e45eb80dbe15ebf3f08f7ce83f3d0a0979c48446
rees_s <simon.rees@psi.ch>
2020-06-01 21:49:20 +0200

    Added testRepeaterBeaconForwarding.

--- src/test/java/org/epics/ca/EpicsChannelAccessTestServerTest.java
+++ src/test/java/org/epics/ca/EpicsChannelAccessTestServerTest.java
@@ -4,10 +4,13 @@ package org.epics.ca;
 
 /*- Imported packages --------------------------------------------------------*/
 
+import org.epics.ca.impl.repeater.CARepeaterStarter;
 import org.epics.ca.util.logging.LibraryLogManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
@@ -30,7 +33,7 @@ public class EpicsChannelAccessTestServerTest
    @BeforeEach
    void beforeEach()
    {
-      System.setProperty( "CA_DEBUG","1" );
+      System.setProperty( "CA_DEBUG", "true" );
    }
 
    @RepeatedTest( 10 )
@@ -58,7 +61,37 @@ public class EpicsChannelAccessTestServerTest
       server.shutdown();
    }
 
-/*- Private methods ----------------------------------------------------------*/
+
+   @Test
+   void testRepeaterBeaconForwarding() throws InterruptedException
+   {
+      // The logging is set to verbose so that the log messages can be examined
+      // by eye to see whether the repeater successfully registered. There should
+      // be messages in the log saying that one client was registered. Then there
+      // should be multiple messages as the test server comes up and sends beacon
+      // messages which the repeater forwards to the CA client library.
+      System.setProperty( "CA_LIBRARY_LOG_LEVEL", Level.ALL.toString() );
+      System.setProperty( "CA_REPEATER_LOG_LEVEL", Level.ALL.toString() );
+      System.setProperty( "CA_REPEATER_OUTPUT_CAPTURE", "true" );
+      System.setProperty( "CA_REPEATER_SHUTDOWN_ON_CONTEXT_CLOSE", "true");
+
+      final EpicsChannelAccessTestServer server;
+      try ( Context context = new Context() )
+      {
+         // Allow one second for the CA library to start a repeater and to register with it.
+         Thread.sleep( 1000 );
+         
+         // Now start the test server which should emit a number of beacon messages
+         // to say that it is alive.
+         server = EpicsChannelAccessTestServer.start();
+         Thread.sleep( 10_000 );
+      }
+      server.shutdown();
+      CARepeaterStarter.shutdownLastStartedRepeater();
+   }
+
+
+   /*- Private methods ----------------------------------------------------------*/
 /*- Nested Classes -----------------------------------------------------------*/
 
 }
