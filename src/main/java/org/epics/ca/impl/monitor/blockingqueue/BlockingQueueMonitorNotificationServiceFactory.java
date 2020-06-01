15b06f5d25a433c399de95ee1dc68ae28c09c859
rees_s <simon.rees@psi.ch>
2020-06-01 18:03:32 +0200

    Optimised imports and now logs closure.

--- src/main/java/org/epics/ca/impl/monitor/blockingqueue/BlockingQueueMonitorNotificationServiceFactory.java
+++ src/main/java/org/epics/ca/impl/monitor/blockingqueue/BlockingQueueMonitorNotificationServiceFactory.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.function.Consumer;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /*- Interface Declaration ----------------------------------------------------*/
