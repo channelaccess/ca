7049cbc0503052ec223f2ba879042fc519c7fdbf
rees_s <simon.rees@psi.ch>
2020-06-08 01:37:56 +0200

    New class for monityoring that tests do not leave dangling threads.

---
+++ src/test/java/org/epics/ca/ThreadWatcher.java
@@ -0,0 +1,128 @@
+/*- Package Declaration ------------------------------------------------------*/
+
+package org.epics.ca;
+
+/*- Imported packages --------------------------------------------------------*/
+
+import net.jcip.annotations.ThreadSafe;
+import org.epics.ca.util.logging.LibraryLogManager;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+
+/*- Interface Declaration ----------------------------------------------------*/
+/*- Class Declaration --------------------------------------------------------*/
+
+@ThreadSafe
+public class ThreadWatcher
+{
+
+/*- Public attributes --------------------------------------------------------*/
+/*- Private attributes -------------------------------------------------------*/
+
+   private static final Logger logger = LibraryLogManager.getLogger( ThreadWatcher.class );
+   private static final Set<String> jvmManagedThreads = new HashSet<>( Arrays.asList( "process reaper", "Finalizer", "Reference Handler", "Signal Dispatcher" ) );
+   private final Set<Thread> threadsAtStart;
+
+/*- Main ---------------------------------------------------------------------*/
+/*- Constructor --------------------------------------------------------------*/
+
+   private ThreadWatcher( Set<Thread> threadsAtStart )
+   {
+      logger.finest( "Threads at start: '" + threadsAtStart + "'" );
+      this.threadsAtStart = threadsAtStart;
+   }
+
+/*- Public methods -----------------------------------------------------------*/
+
+   public static ThreadWatcher start()
+   {
+      return new ThreadWatcher( getStableThreadSnapshot() );
+   }
+
+   public void verify()
+   {
+      final Set<Thread> threadsAtEndTry1 = getStableThreadSnapshot();
+      logger.finest( "Threads at end: try 1: '" + threadsAtEndTry1 + "'" );
+      
+      if ( userThreadsEqual( threadsAtStart, threadsAtEndTry1, jvmManagedThreads ) )
+      {
+         return;
+      }
+      
+      final int longStabilisationDelayInMillis = 500;
+      safeSleep( longStabilisationDelayInMillis );
+      
+      final Set<Thread> threadsAtEndTry2 = getStableThreadSnapshot();
+      logger.finest( "Threads at end: try 1: '" + threadsAtEndTry2 + "'" );
+      if ( userThreadsEqual( threadsAtStart, threadsAtEndTry2, jvmManagedThreads ) )
+      {
+         return;
+      }
+
+      logger.warning( "-- THREAD CHANGES ------------------------------" );
+      logger.info( "-- BEFORE -----------------------------------------" );
+      showThreads( threadsAtStart );
+      logger.info( "-- NOW --------------------------------------------" );
+      showThreads( threadsAtEndTry2 );
+      throw new RuntimeException( "Threads have changed !" );
+   }
+
+   
+/*- Package-level methods ----------------------------------------------------*/
+/*- Private methods ----------------------------------------------------------*/
+
+   private static boolean userThreadsEqual( Set<Thread> before, Set<Thread> after, Set<String> jvmThreadNames )
+   {
+      final Set<Thread> filteredBefore = before.stream().filter( x -> ! jvmThreadNames.contains( x.getName().trim() ) ).collect(Collectors.toSet() );
+      final Set<Thread> filteredAfter = after.stream().filter( x -> ! jvmThreadNames.contains( x.getName().trim() ) ).collect(Collectors.toSet() );
+      return filteredBefore.equals( filteredAfter );
+   }
+
+   private static Set<Thread> getStableThreadSnapshot()
+   {
+      final int shortStabilisationDelayInMillis = 10;
+      
+      Set<Thread> threadsEarlier = getThreadSnapshot();
+      safeSleep( shortStabilisationDelayInMillis );
+      Set<Thread> threadsNow = getThreadSnapshot();
+      while ( ! threadsNow.equals( threadsEarlier ) )
+      {
+         threadsEarlier = threadsNow;  
+         threadsNow = getThreadSnapshot();
+         safeSleep( shortStabilisationDelayInMillis );
+      }
+      return threadsNow;
+   }
+
+   private static Set<Thread> getThreadSnapshot()
+   {
+      return Thread.getAllStackTraces().keySet();
+   }
+
+   private static void safeSleep( int delayInMillis )
+   {
+      try
+      {
+         Thread.sleep( delayInMillis );
+      }
+      catch ( InterruptedException e )
+      {
+         final String message = "Interrupted whilst waiting for threads to stabilise";
+         logger.warning( message );
+         throw new RuntimeException( message );
+      }
+   }   
+   
+   private static void showThreads( Set<Thread> threadSet) 
+   {
+      threadSet.forEach( x -> logger.info( x.getName() ) );
+   }   
+   
+/*- Nested Classes -----------------------------------------------------------*/
+
+}
